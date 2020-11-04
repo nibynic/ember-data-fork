601bd12fd8c036f815d57045c3d92216bb4572b1
Paweł <jembezmamy@users.noreply.github.com>
2020-11-04 14:21:36 +0100

    ember-cli 3.22

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,4 +1,5 @@
 'use strict';
+const getChannelURL = require('ember-source-channel-url');
 
 module.exports = async function() {
   return {
@@ -20,14 +21,36 @@ module.exports = async function() {
           }
         }
       },
-      // The default `.travis.yml` runs this scenario via `npm test`,
-      // not via `ember try`. It's still included here so that running
-      // `ember try:each` manually or from a customized CI config will run it
-      // along with all the other scenarios.
       {
-        name: 'ember-default',
+        name: 'ember-lts-3.16',
         npm: {
-          devDependencies: {}
+          devDependencies: {
+            'ember-source': '~3.16.0'
+          }
+        }
+      },
+      {
+        name: 'ember-lts-3.20',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.20.5'
+          }
+        }
+      },
+      {
+        name: 'ember-release',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('release')
+          }
+        }
+      },
+      {
+        name: 'ember-beta',
+        npm: {
+          devDependencies: {
+            'ember-data': '~3.5.0'
+          }
         }
       },
       {
@@ -39,7 +62,7 @@ module.exports = async function() {
         },
         npm: {
           devDependencies: {
-            '@ember/jquery': '^0.5.1'
+            '@ember/jquery': '^1.1.0'
           }
         }
       }
